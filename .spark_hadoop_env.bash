0b9319d5d69fe6009dc34314e4f19eae74dc75e0
ttsy <japan.t.egawa@gmail.com>
2016-11-26 14:32:08 +0900

    Add .spark_hadoop_env.bash

---
+++ .spark_hadoop_env.bash
@@ -0,0 +1,18 @@
+export OPENSSL_ROOT_DIR=$(brew --prefix openssl)
+export OPENSSL_INCLUDE_DIR=$OPENSSL_ROOT_DIR/include/
+
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+export HADOOP_HOME=/usr/local/hadoop-2.7.3
+export HADOOP_PREFIX=$HADOOP_HOME
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export HADOOP_OPTS="-Djava.library.path=$HADOOP_COMMON_LIB_NATIVE_DIR"
+
+export SPARK_HOME=/usr/local/spark-2.0.2-bin-hadoop2.7
+export PATH=$PATH:$SPARK_HOME/bin
+export PAHT=$PATH:$SPARK_HOME/sbin
